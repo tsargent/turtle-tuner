b48fc97ec9326fe3e6899b69429cefea6a3ec8b9
Tyler Sargent <tsargent@gmail.com>
2023-12-13 21:20:02 -0500

    roation animation

--- src/components/Meter.tsx
+++ src/components/Meter.tsx
@@ -10,37 +10,43 @@ import {
 } from 'react-native';
 
 function Meter(props: {frequency: number}): JSX.Element {
-  const meterAnimation = React.useRef(new Animated.Value(0)).current;
+  const cents = React.useRef(new Animated.Value(0)).current;
   const {frequency} = props;
+  const [centChange, setCentsChange] = React.useState(0);
 
   const goLeft = () => {
-    console.log('goLeft');
-    Animated.timing(meterAnimation, {
-      toValue: 0,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
+    setCentsChange(-10);
   };
 
   const goRight = () => {
-    Animated.timing(meterAnimation, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start();
+    setCentsChange(10);
   };
 
   useEffect(() => {
-    Animated.timing(meterAnimation, {
-      toValue: frequency,
+    console.log('centChange????', centChange);
+    Animated.timing(cents, {
+      toValue: centChange,
       duration: 1000,
       useNativeDriver: true,
     }).start();
-  }, [frequency]);
+  }, [centChange]);
+
+  console.log('cents', cents);
+  console.log('centChange', centChange);
+
+  const interpolated = cents.interpolate({
+    inputRange: [-10, 10],
+    outputRange: ['-45deg', '45deg'],
+  });
+
+  const rotate = cents.interpolate({
+    inputRange: [-10, 10],
+    outputRange: ['-45deg', '45deg'],
+  });
 
   return (
     <View>
-      <Animated.View style={[styles.meter, {opacity: meterAnimation}]} />
+      <Animated.View style={[styles.meter, {transform: [{rotate}]}]} />
       <TouchableOpacity
         onPress={goLeft}
         style={{padding: 20, backgroundColor: 'gray'}}>
